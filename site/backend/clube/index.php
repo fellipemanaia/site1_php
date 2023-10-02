51f5b3a3ad091006dd2bad4372c069aeeee18f71
Fellipe Manaia <100931530+fellipemanaia@users.noreply.github.com>
2023-10-02 17:42:57 -0300

    Site API/PHP VSCode 02/10

---
+++ site/backend/clube/index.php
@@ -0,0 +1,27 @@
+<?php
+    header('Content-type: application/json');
+    $dadosEnviados = file_get_contents('php://input');
+    $dadosEnviados = json_decode($dadosEnviados, true);
+
+    $cxPdo = new PDO('mysql:host=localhost;port=3306;dbname=campeonato_futbol','root','');
+
+    $cmdSql = "SELECT * FROM clube"
+
+    if(isset($dadosEnviados['busca'])){
+        $busca = $dadosEnviados['busca'];
+        $cmdSql = "SELECT * FROM clube WHERE clube.nome LIKE '%$busca%' "; 
+    }
+
+    $cxPrepare = $cxPdo->prepare($cmdSql);
+    $dados = [
+        'result'=>false,
+        'valores' => []
+    ];
+
+    if($cxPrepare->execute()){
+        if($cxPrepare->rowCount() > 0){
+            $dados['result'] = true;
+            $dados['valores'] = $cxPrepare->fetchAll();
+        }
+    }
+    echo json_encode($dados);
